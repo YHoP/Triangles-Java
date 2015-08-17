845e27af9a4b36f66f4388c7546ef8120249d6c7
Guest User <student@epicodus.com>
2015-08-17 12:12:43 -0700

    pass all tests

--- src/test/java/TrianglesTest.java
+++ src/test/java/TrianglesTest.java
@@ -11,7 +11,7 @@ public class TrianglesTest {
 
   @Test
   public void isTriangle_whenTwoSidesSumLessThanThird_false() {
-    Triangles testTriangles = new Triangles(2,9,16);
+    Triangles testTriangles = new Triangles(1,2,3);
     assertEquals(false, testTriangles.isTriangle());
   }
 
@@ -21,4 +21,39 @@ public class TrianglesTest {
     assertEquals(true, testTriangles.isEquilateral());
   }
 
+  @Test
+  public void isIsosceles_whenTwoSidesAreEqual_true() {
+    Triangles testTriangles = new Triangles (2,3,2);
+    assertEquals(true, testTriangles.isIsosceles());
+  }
+
+  @Test
+  public void isScalene_whenNoSidesAreEqual_true() {
+    Triangles testTriangles = new Triangles (2,3,4);
+    assertEquals(true, testTriangles.isScalene());
+  }
+
+  @Test
+  public void triangleType_isReturnedCorrectType_Equilateral(){
+    Triangles testTriangles = new Triangles (2,2,2);
+    assertEquals("Equilateral", testTriangles.triangleType());
+  }
+
+  @Test
+  public void triangleType_isReturnedCorrectType_Isosceles() {
+    Triangles testTriangles = new Triangles (2,2,3);
+    assertEquals("Isosceles", testTriangles.triangleType());
+  }
+
+  @Test
+  public void triangleType_isReturnedCorrectType_Scalene() {
+    Triangles testTriangles = new Triangles (2,3,4);
+    assertEquals("Scalene", testTriangles.triangleType());
+  }
+
+  @Test
+  public void triangleType_isReturnedCorrectType_NotATriangle() {
+    Triangles testTriangles = new Triangles (1,5,20);
+    assertEquals("That's not a triangle! Try again.", testTriangles.triangleType());
+  }
 }
